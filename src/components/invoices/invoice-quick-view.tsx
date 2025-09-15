2f0215c3f09fa459965767caaec29ced9d897088
Nguyen Tri <nneenterpriseinc@gmail.com>
2025-09-15 06:31:53 +0000

    the print still have button inside , you sure it print from template?

--- src/components/invoices/invoice-quick-view.tsx
+++ src/components/invoices/invoice-quick-view.tsx
@@ -19,7 +19,7 @@ export function InvoiceQuickView({ invoice, showRefundExchangeButton = false }: 
 
   return (
     <div className="flex flex-col gap-4">
-      <div className="flex items-center justify-end gap-2">
+      <div className="flex items-center justify-end gap-2 print:hidden">
         {showRefundExchangeButton && (
           <Link href={`/dashboard/invoices/${invoice.id}/refund`} passHref>
              <Button variant="outline" disabled={!canRefund}>
